7c3a6327331f5c8cce1cd74ce130d2ae354a123d
Rami <178672291+RakeemRussell@users.noreply.github.com>
2026-07-05 19:29:46 -0400

    working terraform, files 00-07 build and run

--- 1-lab/1a-terraform/terraform/02-subnets.tf
+++ 1-lab/1a-terraform/terraform/02-subnets.tf
@@ -8,12 +8,21 @@ resource "aws_subnet" "public_subnet_resource" {
   }
 }
 
-### PRIVATE SUBNET ###
-resource "aws_subnet" "private_subnet_resource" {
+### PRIVATE SUBNET A ###
+resource "aws_subnet" "private_subnet_a_resource" {
   vpc_id     = aws_vpc.vpc_resource.id
-  cidr_block = "10.90.11.0/24"
+  cidr_block = "10.90.10.0/24"
   availability_zone = "us-east-1a"
   tags = {
-    Name = "private_subnet_resource"
+    Name = "private_subnet-a_resource"
+  }
+}
+### PRIVATE SUBNET B ###
+resource "aws_subnet" "private_subnet_b_resource" {
+  vpc_id     = aws_vpc.vpc_resource.id
+  cidr_block = "10.90.20.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private_subnet-b_resource"
   }
 }

e68368ecc8ddc258bebc1369ba9630077e1c4377
Rami <178672291+RakeemRussell@users.noreply.github.com>
2026-08-09 00:03:09 -0400

    added .tf files

---
+++ 1-lab/1c_bonus_a/terraform/18-variables.tf
@@ -0,0 +1,27 @@
+variable "email_alert" {
+  description = "Email address to receive error notifications."
+  type        = string
+}
+
+variable "iam_role_auto_grader_arn" {
+  description = "IAM user or role ARN allowed to assume the temporary Lab 1b incident injector role."
+  type        = string
+}
+
+variable "log_group_name" {
+  type    = string
+  default = "/aws/ec2/lab-rds-app"
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "secret_id" {
+  type = string
+}
+
+variable "db_instance_id" {
+  type = string
+}
+
---
+++ 1-lab/1c_bonus_a/terraform/19-outputs.tf
@@ -0,0 +1,36 @@
+output "sns_db_error_tf_topic_arn" {
+  description = "SNS Topic ARN for database incidents."
+  value       = aws_sns_topic.sns_db_error_tf.arn
+}
+
+output "iam_role_auto_grader_arn" {
+  description = "IAM role ARN to assume for temporary Lab 1b incident injection."
+  value       = aws_iam_role.auto_grader.arn
+}
+
+output "gcp_region" {
+  value = var.aws_region
+}
+
+output "gcp_secret_id" {
+  value = aws_secretsmanager_secret.db_secret.name
+}
+
+
+
+output "db_instance_id" {
+  value = aws_db_instance.mysql_rds_db.identifier
+}
+
+output "rds_security_group_id" {
+  value = aws_security_group.sg_private_resource.id
+}
+
+output "ec2SecurityGroupId" {
+  value = aws_security_group.sg_ec2_lab.id
+}
+
+output "ec2_public_ipv4" {
+  description = "Public IPv4 address of the EC2 instance"
+  value       = aws_instance.ec2_public.public_ip
+}

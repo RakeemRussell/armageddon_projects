9df7f1ab94f55e54a1e3818739ecda2b54b15389
Rami <178672291+RakeemRussell@users.noreply.github.com>
2026-09-10 19:33:10 -0400

    updated 00 file

--- 2-lab/2a-lab/terraform/00-auth.tf
+++ 2-lab/2a-lab/terraform/00-auth.tf
@@ -4,9 +4,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.61.0"
     }
+      random = {
+    source  = "hashicorp/random"
+    version = "3.6.3"
+  }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "aws" {
+  alias  = "cloudfront"
+  region = "us-east-1"
 }

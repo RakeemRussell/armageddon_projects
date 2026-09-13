727a68a8829a9bd63da37665f54a7963e241cb51
Rami <178672291+RakeemRussell@users.noreply.github.com>
2026-09-12 20:02:09 -0400

    added rule block to waf acl resource

--- 2-lab/2a-lab/terraform/35-waf-acl.tf
+++ 2-lab/2a-lab/terraform/35-waf-acl.tf
@@ -13,4 +13,26 @@ resource "aws_wafv2_web_acl" "waf_acl" {
     metric_name                = "visibility_metric_name"
     sampled_requests_enabled   = true
   }
+
+  rule {
+    name     = "waf_acl_rule"
+    priority = 0
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "visibility_config_name"
+      sampled_requests_enabled   = true
+    }
+  }
 }

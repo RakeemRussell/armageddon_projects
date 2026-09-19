744b8a26ce58ecd03caae8f4db046bf2669907c5
Rami <178672291+RakeemRussell@users.noreply.github.com>
2026-09-18 21:39:31 -0400

    created 2b auth file

---
+++ 2-lab/2b-lab/terraform/00-auth.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.64.0"
+    }
+        random = {
+      source  = "hashicorp/random"
+      version = "3.9.0"
+    }
+  }
+  }
+
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "aws" {
+  alias  = "cloudfront"
+  region = "us-east-1"
+}
